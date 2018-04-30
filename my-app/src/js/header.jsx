1f18a9e302a43bf43c72ef2ff0a7ee0ff70eb0fa
Maja Anna <majaprzyjazna@gmail.com>
2018-04-30 09:04:59 +0200

    hamburger menu - mobile first

--- my-app/src/js/header.jsx
+++ my-app/src/js/header.jsx
@@ -16,27 +16,71 @@ class Header extends React.Component{
 }
 
 class Navigation extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            showMenu:false,
+            hamburgerShow:true,
+            hideMenu:false
+        }
+    }
+
+    showMenuFunc = () => {
+        console.log('ckklasdn')
+        this.setState({
+            showMenu:this.state.showMenu === true ? false : true,
+            hamburgerShow: this.state.hamburgerShow === true ? false : true,
+            hideMenu: this.state.hideMenu === true ? false : true
+        })
+    }
+
+    hideMenu = () => {
+        console.log('hide')
+        this.setState({
+            showMenu:false,
+            hideMenu:false,
+            hamburgerShow:true
+        })
+    }
+
     render(){
         return(
             <nav>
-                <ul className='menu'>
-                    <li key='home'>
-                        <NavLink exact to='/'
-                                 className='navText'
-                                 activeClassName="active">Home</NavLink>
-                    </li>
-                    <li key='map'>
-                        <NavLink to='/map'
-                                 className='navText'
-                                 activeClassName="active">Mapa</NavLink>
-                    </li>
-                    <li key='aboutProject'>
-                        <NavLink to='/aboutProject'
-                                 className='navText'
-                                 activeClassName="active">O projekcie</NavLink>
-                    </li>
-                </ul>
-                <span className='icon-menu hamburger iconBig' onClick={this.showMenu}></span>
+                {
+                    this.state.showMenu === true ?
+                        <ul className='menu'>
+                            <li key='home'>
+                                <NavLink exact to='/'
+                                         className='navText'
+                                         activeClassName="active">Home</NavLink>
+                            </li>
+                            <li key='map'>
+                                <NavLink to='/map'
+                                         className='navText'
+                                         activeClassName="active">Mapa</NavLink>
+                            </li>
+                            <li key='aboutProject'>
+                                <NavLink to='/aboutProject'
+                                         className='navText'
+                                         activeClassName="active">O projekcie</NavLink>
+                            </li>
+                            {
+                                this.state.hideMenu === true ?
+                                    <li key='hideMenu'>
+                                        <span className='icon-cancel-circled iconBig' onClick={this.hideMenu}></span>
+                                    </li>
+                                    : null
+                            }
+                        </ul>
+                        : null
+                }
+
+                {
+                    this.state.hamburgerShow === true ?
+                        <span className='icon-menu hamburger iconBig' onClick={this.showMenuFunc}></span>
+                        : null
+                }
             </nav>
         )
     }
